c127f159a792433df69e8975220a91684fea4bdb
chu <wuchu9402@gmail.com>
2021-03-30 11:11:50 +0800

    v1.5 a demo version

---
+++ +mascot/polytopeSearch.m
@@ -0,0 +1,49 @@
+function [trajectory,region] = polytopeSearch(envir,robotdes,robotnum,linkscale,dispbaseref,dispendref)
+% polytopeSearch    Generate a collision-free polytope graph by sampling 
+% and search a feasible path. References are listed as follows:
+% [1] J. Alonso-Mora, S. Baker, and D. Rus, ¡°Multi-robot formation control 
+%     and object transport in dynamic environments via constrained optimization,¡± 
+%     Int. J. Rob. Res., vol. 36, no. 9, pp. 1000¨C1021, Aug. 2017.
+% [2] R. Deits and R. Tedrake, ¡°Computing large convex regions of obstacle-free
+%     space through semidefinite programming,¡± in Springer Tracts in Advanced 
+%     Robotics, 2015, vol. 107, pp. 109¨C124.
+
+%% initialize an empty polytope graph
+% z nodes
+nodez = []; % z vector in row
+nodepoly = []; % polytope struct
+nodezinpoly = {}; % z index
+% z configs (angle and size)
+ang = []; siz = 1;
+config.vec = [ang,siz];
+config.lb = [ang-pi,0.2/linkscale];
+config.ub = [ang+pi,1/linkscale];
+% z edges
+edgez = []; % z index
+%% dilate obstacles
+for i=1:length(envir.obstacle)
+    envir.obstacle{i} = dilate(envir.obstacle{i},robotdes.radius);
+end
+%% initialize nodez with start and goal
+[nodez, nodepoly, nodezinpoly] = polyinit(nodez,nodepoly,nodezinpoly,config,envir,robotnum,dispbaseref,dispendref);
+% add goal to graph
+[nodez,nodezinpoly,nodegrid,edgez] = polyadd(nodez,nodepoly,nodezinpoly,edgez,1,config,envir,robotnum,dispbaseref,dispendref);
+% check if there is a feasible path between start(1) and goal(2)
+[noderoute,polyroute] = polyastar(nodez,nodezinpoly,nodegrid,1,2);
+%% add random node to graph
+ctr = 0;
+while isempty(noderoute)
+    % generate 5 node per loop
+    randnum = 1;
+    [nodez,nodepoly,nodezinpoly] = polyrand(nodez,nodepoly,nodezinpoly,randnum,config,envir,robotnum,dispbaseref,dispendref);
+    % create undirected graph and its edges
+    [nodez,nodezinpoly,nodegrid,edgez] = polyadd(nodez,nodepoly,nodezinpoly,edgez,randnum,config,envir,robotnum,dispbaseref,dispendref);
+    [noderoute,polyroute] = polyastar(nodez,nodezinpoly,nodegrid,1,2);
+    ctr = ctr + 1;
+end
+%% generate trajectory and region
+region = nodepoly(polyroute);
+[x,dx,ddx,t] = mstraj_(nodez(noderoute,:),ones(1,size(nodez,2))*2,0.1,2);
+trajectory = struct('pos',x,'dot_pos',dx,...
+    'ddot_pos',ddx,'time',t);
+end
---
+++ option/Replay.m
@@ -0,0 +1,64 @@
+% MASCOT Replay
+close all
+clear
+% load data
+current_path = '..';
+load([current_path '/data/paper_our_simu'],'data');
+% initials
+pos_traj_ref = data(1).pos_traj_ref;
+pos_traj_hat = data(1).pos_traj_hat;
+pos_end = data(1).pos_end;
+opt_state = data(1).opt_state;
+opt_scale = data(1).opt_scale;
+% plot parameters
+is_plotted = struct('robot',true,...
+    'label',true,...
+    'trajectory',true,...
+    'reference',true,...
+    'estimation',false,...
+    'object',true,...
+    'basehat',false,...
+    'cone',false,...
+    'workrange',true,...
+    'obstacle',true,...
+    'region',false,...
+    'axis',false);
+% plot
+dim = 3;
+[scene, uiaxis] = Mascot.scenePlot(pos_traj_ref,pos_traj_hat,pos_end,opt_state,opt_scale,current_path,dim,is_plotted);
+set(gcf,'color',[1,1,1])
+v0 = [0,90]; view(v0);
+vd = [-30 40];
+stage = length(data)/4;
+dv = (vd-v0)/stage;
+% replay
+video_on = false;
+playspeed = 5;
+for loop=1:length(data)
+    t = loop*0.02;
+    % update figure
+    if mod(loop,playspeed)==0
+        % state load
+        pos_traj_ref = data(loop).pos_traj_ref;
+        pos_traj_hat = data(loop).pos_traj_hat;
+        pos_end = data(loop).pos_end;
+        opt_state = data(loop).opt_state;
+        opt_scale = data(loop).opt_scale;
+        % animate
+        scene = Mascot.sceneAnimate(scene,pos_traj_ref,pos_traj_hat,pos_end,opt_state,opt_scale,current_path);
+        if loop>stage*1.5&&loop<stage*2.5
+            view(v0+dv*(loop-stage*1.5));
+        end
+        if loop>stage*2.5&&loop<stage*3.5
+            view(vd-dv*(loop-stage*2.5));
+        end
+        % video
+        if video_on
+            frame(loop/playspeed) = getframe(gcf);
+        end
+        drawnow
+    end
+end
+if video_on
+    savevideo([current_path '/data/video'],frame);
+end
